9ff1fe43d0ffca7b087a5aba6920dde48fe8fed6
Counter2828 <majko2828@gmail.com>
2026-02-17 19:55:26 +0100

    Little bit of refactoring

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,52 +1,44 @@
 namespace Snake
 {
     public class Game{
-        public PlayArea playArea;
-        public Pixel snake { get; set; }
-        public Pixel berry { get; set; }
-        public int score;
-        public bool gameOver;
-        private DateTime startTime;
-        public Direction direction;
+        private readonly PlayArea _playArea;
+        public PlayArea Area => _playArea;
+        public Pixel Snake { get; }
+        public Pixel Berry { get; }
+        public int Score { get; set; }
+        public bool GameOver { get; set; }
+        private readonly DateTime _startTime;
+        private Direction _direction;
+        public Direction CurrentDirection
+        {
+            get => _direction;
+            set => _direction = value;
+        }
 
         public Game() {
-            this.playArea = new PlayArea(64, 48, ConsoleColor.Cyan);
-            this.snake = new Pixel(playArea.size.X/2, playArea.size.Y/2, ConsoleColor.Red);
-            this.berry = new Pixel(0, 0, ConsoleColor.Magenta);
-            this.score = 0;
-            this.gameOver = false;
-            this.startTime = DateTime.Now;
-            this.direction = Direction.Right;
+            _playArea = new PlayArea(64, 48, ConsoleColor.Cyan);
+            Snake = new Pixel(_playArea.Size.X/2, _playArea.Size.Y/2, ConsoleColor.Red);
+            Berry = new Pixel(0, 0, ConsoleColor.Magenta);
+            Score = 0;
+            GameOver = false;
+            _startTime = DateTime.Now;
+            CurrentDirection = Direction.Right;
         }
 
-        public DateTime GetStartTime() { return startTime; }
+        public DateTime StartTime => _startTime;
 
-        public class PlayArea
+        public class PlayArea(int width, int height, ConsoleColor color, char symbol = '■')
         {
-            public Vector2Int size { get; set; }
-            public ConsoleColor color { get; set; }
-            public char symbol { get; set; }
-
-            public PlayArea(int width, int height, ConsoleColor color, char symbol = '■')
-            {
-                this.size = new Vector2Int(width, height);
-                this.color = color;
-                this.symbol = symbol;
-            }
+            public Vector2Int Size { get; set; } = new(width, height);
+            public ConsoleColor Color { get; set; } = color;
+            public char Symbol { get; set; } = symbol;
         }
 
-        public class Pixel
+        public class Pixel(int x, int y, ConsoleColor color, char symbol = '■')
         {
-            public Vector2Int position { get; set; }
-            public ConsoleColor color { get; set; }
-            public char symbol { get; set; }
-
-            public Pixel(int x, int y, ConsoleColor color, char symbol = '■')
-            {
-                this.position = new Vector2Int(x, y);
-                this.color = color;
-                this.symbol = symbol;
-            }
+            public Vector2Int Position { get; set; } = new(x, y);
+            public ConsoleColor Color { get; set; } = color;
+            public char Symbol { get; set; } = symbol;
         }
 
         public enum Direction
@@ -57,31 +49,27 @@ namespace Snake
             Right
         }
 
-        public struct Vector2Int {
-            public int X { get; set; }
-            public int Y { get; set; }
-
-            public Vector2Int(int x, int y) {
-                X = x;
-                Y = y;
-            }
+        public struct Vector2Int(int x, int y)
+        {
+            public int X { get; set; } = x;
+            public int Y { get; set; } = y;
         }
     }
 
     class Program
     {
-        static Game game = new Game();
-        static DateTime lastFrameTime = DateTime.Now;
+        private static readonly Game Game = new Game();
+        private static DateTime _lastFrameTime = DateTime.Now;
 
         /*
          * Refreshes the play area
          */
         static void DrawPlayArea()
         {
-            Console.ForegroundColor = game.playArea.color;
-            int x = game.playArea.size.X;
-            int y = game.playArea.size.Y;
-            char areaBlock = game.playArea.symbol;
+            Console.ForegroundColor = Game.Area.Color;
+            int x = Game.Area.Size.X;
+            int y = Game.Area.Size.Y;
+            char areaBlock = Game.Area.Symbol;
 
             for (int i = 0; i < x; i++)
             {
@@ -109,9 +97,9 @@ namespace Snake
          */
         static void DrawSnake()
         {
-            Console.SetCursorPosition(game.snake.position.X, game.snake.position.Y);
-            Console.ForegroundColor = game.snake.color;
-            Console.Write(game.snake.symbol);
+            Console.SetCursorPosition(Game.Snake.Position.X, Game.Snake.Position.Y);
+            Console.ForegroundColor = Game.Snake.Color;
+            Console.Write(Game.Snake.Symbol);
         }
 
         /*
@@ -119,16 +107,16 @@ namespace Snake
          */
         static void DrawBerry()
         {
-            Console.SetCursorPosition(game.berry.position.X, game.berry.position.Y);
-            Console.ForegroundColor = game.berry.color;
-            Console.Write(game.berry.symbol);
+            Console.SetCursorPosition(Game.Berry.Position.X, Game.Berry.Position.Y);
+            Console.ForegroundColor = Game.Berry.Color;
+            Console.Write(Game.Berry.Symbol);
         }
 
-        static void IsGameOver()
+        private static void IsGameOver()
         {
-            if (game.snake.position.X >= game.playArea.size.X || game.snake.position.X <= 0 || game.snake.position.Y >= game.playArea.size.Y || game.snake.position.Y <= 0)
+            if (Game.Snake.Position.X >= Game.Area.Size.X || Game.Snake.Position.X <= 0 || Game.Snake.Position.Y >= Game.Area.Size.Y || Game.Snake.Position.Y <= 0)
             {
-                game.gameOver = true;
+                Game.GameOver = true;
             }
         }
 
@@ -136,9 +124,9 @@ namespace Snake
         static bool IsItNewFrameYet(int milliseconds = 500)
         {
             DateTime frameTime = DateTime.Now;
-            if (frameTime.Subtract(lastFrameTime).TotalMilliseconds >= milliseconds)
+            if (frameTime.Subtract(_lastFrameTime).TotalMilliseconds >= milliseconds)
             {
-                lastFrameTime = frameTime;
+                _lastFrameTime = frameTime;
                 return true;
             }
             return false;
@@ -151,13 +139,13 @@ namespace Snake
         {
             Random randomNumber = new Random();
 
-            var tempPosition = game.berry.position;
+            var tempPosition = Game.Berry.Position;
             tempPosition.X = randomNumber.Next(1, x);
             tempPosition.Y = randomNumber.Next(1, y);
-            game.berry.position = tempPosition;
+            Game.Berry.Position = tempPosition;
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
 
             List<int> xBorder = new List<int>();
@@ -172,27 +160,27 @@ namespace Snake
 
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                if (game.berry.position.X == game.snake.position.X && game.berry.position.Y == game.snake.position.Y)
+                if (Game.Berry.Position.X == Game.Snake.Position.X && Game.Berry.Position.Y == Game.Snake.Position.Y)
                 {
-                    game.score++;
-                    SpawnBerry(game.playArea.size.X, game.playArea.size.Y);
+                    Game.Score++;
+                    SpawnBerry(Game.Area.Size.X, Game.Area.Size.Y);
                 }
                 DrawBerry();
                 for (int i = 0; i < xBorder.Count(); i++)
                 {
                     Console.SetCursorPosition(xBorder[i], yBorder[i]);
                     Console.Write("■");
-                    if (xBorder[i] == game.snake.position.X && yBorder[i] == game.snake.position.Y)
+                    if (xBorder[i] == Game.Snake.Position.X && yBorder[i] == Game.Snake.Position.Y)
                     {
-                        game.gameOver = true;
+                        Game.GameOver = true;
                     }
                 }
-                if (game.gameOver)
+                if (Game.GameOver)
                 {
-                    Console.SetCursorPosition(game.playArea.size.X / 3, (game.playArea.size.Y / 2));
-                    Console.WriteLine("Game over, Score: " + game.score);
-                    Console.SetCursorPosition(game.playArea.size.X / 3, (game.playArea.size.Y / 2) + 1);
-                    Console.SetCursorPosition(0, game.playArea.size.Y + 1);
+                    Console.SetCursorPosition(Game.Area.Size.X / 3, (Game.Area.Size.Y / 2));
+                    Console.WriteLine("Game over, Score: " + Game.Score);
+                    Console.SetCursorPosition(Game.Area.Size.X / 3, (Game.Area.Size.Y / 2) + 1);
+                    Console.SetCursorPosition(0, Game.Area.Size.Y + 1);
                     Console.WriteLine("Press any key to exit...");
 
                     Console.ReadKey();
@@ -209,26 +197,26 @@ namespace Snake
                         ConsoleKeyInfo pressed = Console.ReadKey(true);
                         switch(pressed.Key){
                             case ConsoleKey.UpArrow:
-                                game.direction = Game.Direction.Up;
+                                Game.CurrentDirection = Game.Direction.Up;
                                 break;
                             case ConsoleKey.DownArrow:
-                                game.direction = Game.Direction.Down;
+                                Game.CurrentDirection = Game.Direction.Down;
                                 break;
                             case ConsoleKey.LeftArrow:
-                                game.direction = Game.Direction.Left;
+                                Game.CurrentDirection = Game.Direction.Left;
                                 break;
                             case ConsoleKey.RightArrow:
-                                game.direction = Game.Direction.Right;
+                                Game.CurrentDirection = Game.Direction.Right;
                                 break;
                         }
                     }
 
                     Thread.Sleep(1);
                 }
-                xBorder.Add(game.snake.position.X);
-                yBorder.Add(game.snake.position.Y);
-                var snakePos = game.snake.position;
-                switch (game.direction)
+                xBorder.Add(Game.Snake.Position.X);
+                yBorder.Add(Game.Snake.Position.Y);
+                var snakePos = Game.Snake.Position;
+                switch (Game.CurrentDirection)
                 {
                     case Game.Direction.Up:
                         snakePos.Y--;
@@ -243,8 +231,8 @@ namespace Snake
                         snakePos.X++;
                         break;
                 }
-                game.snake.position = snakePos;
-                if (xBorder.Count() > game.score)
+                Game.Snake.Position = snakePos;
+                if (xBorder.Count() > Game.Score)
                 {
                     xBorder.RemoveAt(0);
                     yBorder.RemoveAt(0);
